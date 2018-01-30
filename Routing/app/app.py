07bc13271f716bee08399cdf857954903de963ed
“vijayasais” <vijay.sai005@gmail.com>
2018-01-31 01:42:39 +0530

    added code new_page for bearing and handled height and width prob for all device

--- Routing/app/app.py
+++ Routing/app/app.py
@@ -23,6 +23,8 @@ geolocator = Nominatim()
 from geo_utils import GeoUtils
 from waymap import WayMap
 
+from PyQt5 import QtWidgets
+
 wmap = WayMap()
 utils = GeoUtils()
 
@@ -63,7 +65,7 @@ def calculate_route():
         destination = request.form['dest'].lower()
         mode = "driving"
 
-        command = "python ../vplot/__main__.py {} {} {}".format(source, destination, mode)
+        command = "python ../vplot/__main__.py \"{}\" \"{}\" \"{}\"".format(source, destination, mode)
         os.system("cd templates/ && rm -rf route_map*")
         os.system(command)
         route = "{}_{}".format(source, destination)
@@ -83,6 +85,13 @@ def haversine_calculator():
 
 @app.route('/utilities/haversine/', methods = ['POST'])
 def haversine_result():
+
+    app_ = QtWidgets.QApplication(sys.argv)
+    screen = app_.primaryScreen()
+    rect = screen.availableGeometry()
+    height = rect.height()
+    width = rect.width()
+
     condition=True
     source = request.form['source']
     dest = request.form['dest']
@@ -158,7 +167,7 @@ def haversine_result():
         center_lng = (source_long + dest_long) / 2.0
 
         trdmap = Map( identifier="trdmap", varname="trdmap", 
-        style="height:592px;width:1300px;margin:0;", 
+        style="height:{}px;width:{}px;margin:0;".format(height,width), 
         zoom=6,
         lat=center_lat, 
         lng=center_lng,
@@ -186,6 +195,12 @@ def old_haversine_calculator():
 
 @app.route('/utilities/haversine/old/', methods = ['POST'])
 def old_haversine_result():
+    app_ = QtWidgets.QApplication(sys.argv)
+    screen = app_.primaryScreen()
+    rect = screen.availableGeometry()
+    height = rect.height()
+    width = rect.width()
+    
     condition=True
     source_lat = request.form['source-lat']
     source_long = request.form['source-long']
@@ -224,7 +239,7 @@ def old_haversine_result():
         center_lng = (source_long + dest_long) / 2.0
 
         trdmap = Map( identifier="trdmap", varname="trdmap", 
-        style="height:592px;width:1300px;margin:0;", 
+        style="height:{}px;width:{}px;margin:0;".format(height,width), 
         zoom=6,
         lat=center_lat, 
         lng=center_lng,
@@ -246,13 +261,136 @@ def old_haversine_result():
         return render_template('haversine.html', result="Invalid LatLng", source_lat=source_lat, source_lng=source_long,\
                        dest_lat=dest_lat, dest_lng=dest_long)
 
-
 @app.route('/utilities/bearing/')
 def bearing_calculator():
-    return render_template('bearing.html')
+    return render_template('bearing_advanced.html')
 
 @app.route('/utilities/bearing/', methods=['POST'])
 def bearing_result():
+
+    app_ = QtWidgets.QApplication(sys.argv)
+    screen = app_.primaryScreen()
+    rect = screen.availableGeometry()
+    height = rect.height()
+    width = rect.width()
+
+    condition=True
+    source = request.form['source']
+    dest = request.form['dest']
+
+    try:
+        try:
+            source_ = source.split(",")
+
+            source_lat = float(source_[0])
+            source_long = float(source_[1])
+
+            dest_ = dest.split(",")
+            dest_long = float(dest_[0])
+
+            dest_lat = float(dest_[1])
+
+            result = utils.compass((source_lat, source_long), (dest_lat, dest_long))
+            out = "Bearing: {} degrees. {}\nDirection: Pointing towards {}"\
+                .format(round(result["angles"]["degrees"], 3), result["reference"], result["directions"]["long"])
+
+            df = pd.DataFrame()
+            df["latitude"] = [source_lat, dest_lat]
+            df["longitude"] = [source_long, dest_long]
+            df["type"] = ["source", "destination"]
+            df["bearing angle"] = [0, round(result["angles"]["degrees"], 3)]
+            df["direction"] = ["", result["directions"]["long"]]
+            data = wmap.html_handling(df, "random")
+
+        except Exception:
+
+            try:
+                check = source.split(",")
+                check = "".join(check)
+                float(check)
+                raise Exception
+            except ValueError:
+                pass
+
+            try:
+                check = dest.split(",")
+                check = "".join(check)
+                float(check)
+                raise Exception
+            except ValueError:
+                pass
+
+            source_result = geolocator.geocode(source)
+            dest_result = geolocator.geocode(dest)
+
+            source_lat = float(source_result.latitude)
+            source_long = float(source_result.longitude)
+            dest_lat = float(dest_result.latitude)
+            dest_long = float(dest_result.longitude)
+
+            result = utils.compass((source_lat, source_long), (dest_lat, dest_long))
+            out = "Bearing: {} degrees. {}\nDirection: Pointing towards {}"\
+                .format(round(result["angles"]["degrees"], 3), result["reference"], result["directions"]["long"])
+            
+            df = pd.DataFrame()
+            df["latitude"] = [source_lat, dest_lat]
+            df["longitude"] = [source_long, dest_long]
+            df["type"] = ["source", "destination"]
+            df["bearing angle"] = [0, round(result["angles"]["degrees"], 3)]
+            df["direction"] = ["", result["directions"]["long"]]
+            data = wmap.html_handling(df, "random")
+
+    except Exception:
+        condition=False
+        pass
+
+    if request.form['action'] == 'submit':
+        try:
+            output = out
+        except Exception:
+            return render_template('bearing_advanced.html', result="Invalid LatLng", source=source, dest=dest)
+        return render_template('bearing_advanced.html', result=output, source=source, dest=dest)
+    
+    elif request.form['action'] == "Show on Map" and condition == True:
+        center_lat = (source_lat + dest_lat) / 2.0
+        center_lng = (source_long + dest_long) / 2.0
+        trdmap = Map( identifier="trdmap", varname="trdmap", 
+        style="height:{}px;width:{}px;margin:0;".format(height,width), 
+        zoom=6,
+        lat=center_lat, 
+        lng=center_lng,
+        markers=[
+            {
+                'icon': icons.dots.green,
+                'lat': source_lat,
+                'lng': source_long,
+                'infobox': data[0]
+            },
+            {
+                'icon': icons.dots.red,
+                'lat': dest_lat,
+                'lng': dest_long,
+                'infobox': data[1]
+            }])
+        return render_template('bearing_map.html', trdmap=trdmap)
+    
+    else:
+        return render_template('bearing_advanced.html', result="Invalid LatLng", source=source, dest=dest)
+
+
+@app.route('/utilities/bearing/old/')
+def old_bearing_calculator():
+    return render_template('bearing.html')
+
+@app.route('/utilities/bearing/old/', methods=['POST'])
+def old_bearing_result():
+
+    app_ = QtWidgets.QApplication(sys.argv)
+    screen = app_.primaryScreen()
+    rect = screen.availableGeometry()
+    height = rect.height()
+    width = rect.width()
+
     condition=True
     source_lat = request.form['source-lat']
     source_long = request.form['source-long']
@@ -265,13 +403,16 @@ def bearing_result():
         dest_lat = float(dest_lat)
         dest_long = float(dest_long)
 
-        result = utils.bearing_angle((source_lat, source_long), (dest_lat, dest_long))
+        result = utils.compass((source_lat, source_long), (dest_lat, dest_long))
+        out = "Bearing: {} degrees. {}\nDirection: Pointing towards {}"\
+            .format(round(result["angles"]["degrees"], 3), result["reference"], result["directions"]["long"])
 
         df = pd.DataFrame()
         df["latitude"] = [source_lat, dest_lat]
         df["longitude"] = [source_long, dest_long]
         df["type"] = ["source", "destination"]
-        df["bearing angle"] = [0, result[0]]
+        df["bearing angle"] = [0, round(result["angles"]["degrees"], 3)]
+        df["direction"] = ["", result["directions"]["long"]]
         data = wmap.html_handling(df, "random")
     except Exception:
         condition=False
@@ -279,7 +420,7 @@ def bearing_result():
 
     if request.form['action'] == 'submit':
         try:
-            output = "Bearing: {} degrees. Clockwise from North".format(round(result[0],3))
+            output = out
         except Exception:
             return render_template('bearing.html', result="Invalid LatLng",  source_lat=source_lat, source_lng=source_long,\
                        dest_lat=dest_lat, dest_lng=dest_long)
@@ -291,7 +432,7 @@ def bearing_result():
         center_lng = (source_long + dest_long) / 2.0
 
         trdmap = Map( identifier="trdmap", varname="trdmap", 
-        style="height:592px;width:1300px;margin:0;", 
+        style="height:{}px;width:{}px;margin:0;".format(height,width), 
         zoom=6,
         lat=center_lat, 
         lng=center_lng,
@@ -321,6 +462,13 @@ def geocode():
 
 @app.route('/utilities/find_lat_lng/', methods=['POST'])
 def geocode_result():
+    
+    app_ = QtWidgets.QApplication(sys.argv)
+    screen = app_.primaryScreen()
+    rect = screen.availableGeometry()
+    height = rect.height()
+    width = rect.width()
+
     place_name = request.form['place']
     condition=True
     try:
@@ -350,7 +498,7 @@ def geocode_result():
             return render_template('geocode.html', result="Invalid place name", place_name=place_name)
 
         trdmap = Map( identifier="trdmap", varname="trdmap", 
-        style="height:592px;width:1300px;margin:0;", 
+        style="height:{}px;width:{}px;margin:0;".format(height,width), 
         zoom=6,
         lat=center_lat, 
         lng=center_lng,
@@ -372,6 +520,13 @@ def reverse_geocode():
 
 @app.route('/utilities/find_address/', methods=['POST'])
 def reverse_geocode_result():
+
+    app_ = QtWidgets.QApplication(sys.argv)
+    screen = app_.primaryScreen()
+    rect = screen.availableGeometry()
+    height = rect.height()
+    width = rect.width()
+
     lat = request.form['lat']
     lng = request.form['lng']
     condition = True
@@ -403,7 +558,7 @@ def reverse_geocode_result():
         center_lng = lng
 
         trdmap = Map( identifier="trdmap", varname="trdmap", 
-        style="height:592px;width:1300px;margin:0;", 
+        style="height:{}px;width:{}px;margin:0;".format(height,width), 
         zoom=6,
         lat=center_lat, 
         lng=center_lng,
